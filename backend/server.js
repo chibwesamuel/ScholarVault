9f36252bad87df3ea3b11c97dcfb6f03bc94e608
chibwesamuel <smchibwe@yahoo.com>
2024-03-09 19:54:57 +0200

    Updated server.js

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,21 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-const app = express()
+const app = express();
 
-const PORT = process.env.PORT || 5000;
+// Middlewares
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+// Routes
+app.get("/", (req, res) => {
+    res.send("Home Page");
+});
 
 // Connecting the DB and starting the server
+const PORT = process.env.PORT || 5000;
+
 mongoose
     .connect(process.env.MONGO_URI)
     .then(() => {
