da9d4b99202b7384c40b13c6778d2f250ce16dbe
chibwesamuel <smchibwe@yahoo.com>
2024-03-13 19:22:50 +0200

    User control changes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,5 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
-const bcrypt = require("bcryptjs");
 
 const registerUser = asyncHandler( async (req, res) => {
     const {name, email, password} = req.body
@@ -23,15 +22,11 @@ const registerUser = asyncHandler( async (req, res) => {
         throw new Error("This email has already been registered")
     }
 
-    // Encrypt password before saving to Database
-    const salt = await bcrypt.genSalt(10)
-    const hashedPassword = await bcrypt.hash(password, salt)
-
     // Create new user
     const user = await User.create({
         name,
         email,
-        password: hashedPassword,
+        password,
     })
     if (user) {
         const { _id, name, email, photo, phone, bio} = user
