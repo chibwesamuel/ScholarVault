bec44d7e6d164561ad6038faec667f8407d02573
chibwesamuel <smchibwe@yahoo.com>
2024-03-13 19:44:14 +0200

    added token auth

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,11 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+
+
+const generateToken = (id) => {
+    return jwt.sign({id}, process.env.JWT_SECRET, {expiresIn: "1d"})
+}
 
 const registerUser = asyncHandler( async (req, res) => {
     const {name, email, password} = req.body
@@ -28,10 +34,20 @@ const registerUser = asyncHandler( async (req, res) => {
         email,
         password,
     })
+
+        // Generate token
+        const token = generateToken(user._id)
+
     if (user) {
         const { _id, name, email, photo, phone, bio} = user
         res.status(201).json({
-            _id: user.id, name, email, photo, phone, bio
+            _id,
+            name,
+            email,
+            photo,
+            phone,
+            bio,
+            token
         })
     } else {
         res.status(400)
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -43,14 +43,14 @@ const userSchema = mongoose.Schema({
 // Encrypt password before sending to DB
 userSchema.pre("save", async function(next) {
     if(!this.isModified("password")) {
-        return next()
+        return next();
     }
 
     // Encrypt password before saving to Database
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(this.password, salt);
     this.password = hashedPassword
-    next()
+    next();
 })
 
 const User = mongoose.model("User", userSchema)
