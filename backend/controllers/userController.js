e06c059011d9046f98175833f4bc0c400e430229
chibwesamuel <smchibwe@yahoo.com>
2024-03-14 09:08:16 +0200

    Middleware set straight

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -131,7 +131,22 @@ const logout = asyncHandler (async (req, res) => {
 
 // Get user data
 const getUser = asyncHandler (async (req, res) => {
-    res.send("Get user data");
+    const user = await User.findById(req.user._id);
+
+    if (user) {
+        const { _id, name, email, photo, phone, bio} = user;
+        res.status(200).json({
+            _id,
+            name,
+            email,
+            photo,
+            phone,
+            bio,
+        })
+    } else {
+        res.status(400)
+        throw new Error("No such user found")
+    }
 });
 
 module.exports = {
