f8889a95d4c986f442c7b82ac2011db967c194f7
chibwesamuel <smchibwe@yahoo.com>
2024-03-30 07:55:36 +0200

    Creation of LoginSignUp

--- scholar-vault-frontend/src/components/LoginSignup/LoginSignup.jsx
+++ scholar-vault-frontend/src/components/LoginSignup/LoginSignup.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import './LoginSignup.css';
 
-import user_icon from '../Assets/person.png'
-import email_icon from '../Assets/email.png'
-import password_icon from '../Assets/password.png'
 const LoginSignup = () => {
     return (
         <div className='container'>
