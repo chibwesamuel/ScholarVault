2c42cdb8c1c56f59d07a0a6b5050e5cce7089da6
chibwesamuel <smchibwe@yahoo.com>
2024-05-21 10:53:18 +0200

    Updates home & index

--- index.js
+++ index.js
@@ -5,12 +5,11 @@ var mongoose = require("mongoose");
 const app = express();
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
 
 mongoose.connect('mongodb+srv://smchibwe:smchibwe@cluster0.zl18dpk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/scholarvault', { useNewUrlParser: true, useUnifiedTopology: true });
+
 var db = mongoose.connection;
 db.on('error', () => console.log("Error in connecting to database"));
 db.once('open', () => console.log("Connected to database"));
@@ -26,6 +25,8 @@ const User = mongoose.model('User', userSchema);
 
 // Signup route
 app.post("/sign_up", async (req, res) => {
+    console.log(req.body); // Log the request body
+
     var { name, email, gender, password } = req.body;
 
     try {
@@ -37,7 +38,7 @@ app.post("/sign_up", async (req, res) => {
         });
         await newUser.save();
         console.log("User created successfully!");
-        return res.redirect('home.html');
+        return res.redirect('/home.html');
     } catch (err) {
         console.error(err);
         res.status(500).send("Error creating user");
@@ -52,7 +53,7 @@ app.post("/login", async (req, res) => {
         const user = await User.findOne({ email, password });
         if (user) {
             console.log("User logged in successfully!");
-            return res.redirect('dashboard.html'); // Redirect to dashboard after successful login
+            return res.redirect('/home.html'); // Redirect to home after successful login
         } else {
             console.log("Invalid email or password");
             return res.status(400).send("Invalid email or password");
